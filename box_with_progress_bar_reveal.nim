9d69cfcf16ab540e1c64c17b73b7bf24bb0808a4
gavr <gavr123456789@gmail.com>
2021-07-20 22:38:52 +0300

    remove unused type

---
+++ box_with_progress_bar_reveal.nim
@@ -0,0 +1,25 @@
+import gintro/[gtk4, gobject, gio, adw]
+
+
+type 
+  BoxWithProgressBarReveal* = ref object of Box
+    revealer*: Revealer
+
+proc createBoxWithProgressBarReveal*(revealOpened: bool): BoxWithProgressBarReveal = 
+  result = newBox(BoxWithProgressBarReveal, Orientation.vertical, 0)
+  let
+    reveal = newRevealer() 
+    progressBar = newProgressBar()
+  progressBar.fraction = 1.0
+  
+  result.revealer = reveal
+  reveal.transitionType = RevealerTransitionType.swingUp
+  reveal.transitionDuration = 200
+  reveal.revealChild = revealOpened
+  reveal.setChild progressBar
+  result.append reveal
+
+  result.vexpand = true
+
+func `showProgressBar=`*(self: BoxWithProgressBarReveal, revealChild: bool) = 
+  self.revealer.revealChild = revealChild
