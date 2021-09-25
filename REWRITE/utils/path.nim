d59baeb0b8e2b9da0d93af4d770a6068b0e5f450
gavr <gavr123456789@gmail.com>
2021-09-25 05:39:07 +0300

    path widget and path controller

---
+++ REWRITE/utils/path.nim
@@ -0,0 +1,64 @@
+import os, strutils
+
+type
+  GlobalPath* = ref object
+    path: string
+
+proc addToPath*(self: GlobalPath, dirOrFile: string) =
+  self.path = self.path / dirOrFile
+
+proc insertPath*(self: GlobalPath, path: string) =
+# сравниваем каждый из элементов текущего пути и пришедшего
+# начиная с первого отличающегося заменяем каждый последующий
+  var selfSplitted = self.path.split DirSep
+  let pathSplitted = path.split DirSep
+  var diffStartFrom: int
+
+  for i in 0..selfSplitted.high:
+    if selfSplitted[i] != pathSplitted[i]:
+      diffStartFrom = i
+  
+  if diffStartFrom != 0:
+    for i in 0..diffStartFrom:
+      selfSplitted[i] = pathSplitted[i]
+  
+  self.path = selfSplitted.join $DirSep
+
+
+proc setPath*(self: GlobalPath, path: static string) =
+  static: assert DirSep in path
+  self.path = path
+
+
+# /1/2/3, get 2 will return /1/2
+proc getNPath*(self: GlobalPath, n: int): string =
+  let x = self.path.split DirSep
+  var y: seq[string]
+  if x.len == 1:
+    return x[0]
+
+  assert x.len >= n
+  for i in 0..<n:
+    y.add x[i]
+
+  result = y.join $DirSep
+
+
+
+when isMainModule:
+  let globalPath = GlobalPath()
+  globalPath.setPath "a/b/c"
+  assert globalPath.path == "a/b/c"
+
+  globalPath.addToPath "d"
+  assert globalPath.path == "a/b/c/d"
+
+  assert (globalPath.getNPath 3) == "a/b/c"
+  assert (globalPath.getNPath 2) == "a/b"
+
+  globalPath.insertPath("a/b/q/w/e")
+
+
+
+
+
--- REWRITE/widgets/path.nim
+++ REWRITE/widgets/path.nim
@@ -1,25 +1,94 @@
 import gintro/[gtk4, gobject, gio, pango, glib, adw]
-import json
-
-let x = json.JObject
+import os, strutils, std/with
+import ../utils/path
 
 type 
   PathWidget* = ref object of Box
     backBtn*: Button
     entry*: Entry
+    path: GlobalPath
+  
+  ToggleWithNum = ref object of ToggleButton
+    num: int
 
-# back btn, Stack((lidt of pathBtn)(text input))
-proc createPathWidget*(dir: string): PathWidget = 
+  Data = tuple
+    toggleBtn: ToggleWithNum
+    path: GlobalPath
+
+  MiddleClick = proc (self: GestureClick, nPress: int, x: cdouble, y: cdouble, arg: ToggleWithNum){.closure.}
+
+
+
+proc gestureMiddleClickCb(self: GestureClick, nPress: int, x: cdouble, y: cdouble, data: Data) =
+  echo data.path.getNPath data.toggleBtn.num + 1
+
+
+proc addMiddleClick(toggleBtn: ToggleWithNum, path: GlobalPath ) =
+  let gestureClick = newGestureClick()
+  gestureClick.setButton(2)
+  gestureClick.connect("pressed", gestureMiddleClickCb, (toggleBtn, path)) # TODO fun
+  toggleBtn.addController(gestureClick)
+
+proc createPathWidget*(path: static string): PathWidget = 
+  static: assert DirSep in path
+
   let 
     pathWidget = newBox(PathWidget, Orientation.horizontal, 0)
-    pathEntry = newEntry()
-  
-  
+
   pathWidget.cssClasses = "linked"
-
-  pathWidget.append pathEntry
-
-
-
   result = pathWidget
 
+  result.path = GlobalPath()
+  result.path.setPath path
+    
+  let 
+    splittedPath = path.split DirSep
+
+  var prevToggleBtn = newToggleButton(ToggleWithNum, splittedPath[0])
+  prevToggleBtn.addMiddleClick(result.path)
+  prevToggleBtn.num = 0
+
+  pathWidget.append prevToggleBtn
+
+  for i, str in splittedPath[1..^1]:
+    let 
+      toggleBtn = newToggleButton(ToggleWithNum, str)
+    toggleBtn.addMiddleClick(result.path)
+    toggleBtn.num = i + 1
+
+    toggleBtn.group = prevToggleBtn
+    prevToggleBtn = toggleBtn
+
+    pathWidget.append toggleBtn
+  
+
+proc addToPathWidget(self: PathWidget, path: string) = 
+  self.path.addToPath path
+  let tglBtn = newToggleButton()
+  # ...
+
+when isMainModule:
+  proc activate(app: gtk4.Application) =
+    let
+      window = adw.newApplicationWindow(app)
+      mainBox = newBox(Orientation.vertical, 5)
+      
+      header = adw.newHeaderBar()
+
+    with mainBox: 
+      append header
+      append createPathWidget("a/b/c/d.txt")
+  
+
+    with window:
+      child = mainBox
+      title = "Katana path widget"
+      defaultSize = (100, 100)
+      show
+
+  proc main() =
+    let app = newApplication("org.gtk.example")
+    app.connect("activate", activate)
+    discard run(app)
+  
+  main()
