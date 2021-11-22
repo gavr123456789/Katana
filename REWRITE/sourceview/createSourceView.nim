eea67c47fff80155aaef208c79a75900181e5cd2
gavr <gavr123456789@gmail.com>
2021-11-22 03:08:30 +0300

    update source view

--- REWRITE/sourceview/createSourceView.nim
+++ REWRITE/sourceview/createSourceView.nim
@@ -1,33 +1,21 @@
 import gintro/[gtk4, gobject, gio, pango, adw, glib, gdk4, gtksource5]
 import std/with
+import createWordCompletion
 
 proc createSourceView*(): gtksource5.View =
   let 
     buffer = newBuffer()
-    # sourceView =
-  result = newViewWithBuffer(buffer)
-
-
-
-proc getCompletionBtnCb(btn: Button) = 
-  echo "sas"
-
-proc createPage*(getCompletitionBtn: Button): ScrolledWindow = 
-  let 
-    sourceView = createSourceView()
+    sourceView = newViewWithBuffer(buffer)
     sourceCompletion = sourceView.completion
     sourceBuffer = sourceView.buffer.Buffer
-    scrolledWindow = newScrolledWindow()
 
     langManager = newLanguageManager()
     lang = langManager.getLanguage("typescript-jsx")
 
-    wordsComplitter = newCompletionWords("words")
 
   # var lang = guessLanguage(langManager, files[0].path, nil)
   # add word provider
-  sourceCompletion.addProvider wordsComplitter
-  wordsComplitter.registerCompletionWords(sourceBuffer)
+  createWordCompletion(sourceView, sourceCompletion)
 
   # add syntax highlight
   echo langManager.getLanguageIds()
@@ -40,19 +28,37 @@ proc createPage*(getCompletitionBtn: Button): ScrolledWindow =
     showLineNumbers = true
     monospace = true
     autoIndent = true
-    leftMargin = 2
+    leftMargin = 40
     rightMargin = 2
     hexpand = true
     vexpand = true
-  # with scrolledWindow:
-  #   hexpand = true
-  #   vexpand = true
-  
+    smartBackspace = true
+    smartHomeEnd = SmartHomeEndType.always
+
+  result = sourceView
+
+
+
+proc getCompletionBtnCb(btn: Button) = 
+  echo "sas"
+
+proc createPage*(getCompletitionBtn: Button): Box = 
+  let 
+    sourceView = createSourceView()
+    scrolledWindow = newScrolledWindow()
+    sourceViewWithPreviewBox = newBox(Orientation.horizontal, 0)
+    # sourceStyleSchemePreview = newSourceStyleSchemePreview()
+
+
+  with sourceViewWithPreviewBox:
+    append scrolledWindow
+    # append sourceStyleSchemePreview
+
   # connects
   getCompletitionBtn.connect("clicked", getCompletionBtnCb)
 
   scrolledWindow.child = sourceView
-  result = scrolledWindow
+  result = sourceViewWithPreviewBox
 
 
 
